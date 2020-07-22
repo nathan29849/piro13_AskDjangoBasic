7d4f33a7446dd977bdf39b856b3baeb23abb6495
nathan29849 <gkstjr5731@hufs.ac.kr>
2020-07-22 15:46:50 +0900

    05 URLConf & 정규표현식

--- askcompany/shop/views.py
+++ askcompany/shop/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def archives_year(request, year):
+    return HttpResponse('{}년도에 대한 내용'.format(year))
