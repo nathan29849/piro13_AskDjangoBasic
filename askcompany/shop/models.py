227c2df62103ea65630b9d1a3b912932ef713cd4
nathan29849 <gkstjr5731@hufs.ac.kr>
2020-07-23 00:49:05 +0900

    9강

--- askcompany/shop/models.py
+++ askcompany/shop/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=100) # Charfield는 길이 제한이 있음
+    desc = models.TextField(blank=True) # TextField는 길이 제한이 없음
+    price = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True) # 추가 될 때 자동 저장
+    updated_at = models.DateTimeField(auto_now=True) # 업데이트 될 때마다 자동 저장
